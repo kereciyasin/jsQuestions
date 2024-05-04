e4c8f36e5e23d6244b4b9b2507b771915bb70be1
Yasin Kereci <kereciyasin52@gmail.com>
2024-05-04 17:22:54 +0200

    questions

---
+++ leap_year_question/conditions.js
@@ -0,0 +1,16 @@
+// * "Leap year question" is a term used to denote years where the number of days in a calendar year is one day more than usual. This extra day is achieved by adding February 29th to the normally 28-day long February. To determine if a year is a leap year, the following algorithm is used: For a year to be a leap year, it must satisfy the following two conditions:
+
+// * 1-The year must be divisible by 4.
+// * 2-The year must not be divisible by 100, unless it is also divisible by 400.
+
+// * Apart from these conditions, in years that do not meet these criteria, February has 28 days, while in leap years, it has 29 days. Given this information, write a code in JavaScript to determine if a year is a leap year or not.
+
+
+
+
+
+
+
+
+
+
