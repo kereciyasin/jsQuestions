54a848b642b515325dbe4fc2d12dd74c85b92e8b
Yasin Kereci <kereciyasin52@gmail.com>
2024-05-06 18:05:25 +0200

    v1

--- prime_number/prime_number.js
+++ prime_number/prime_number.js
@@ -1,1 +1,12 @@
 //* The code that writes whether the entered number is a prime number or not.
+
+const number = +prompt("Please enter a positive number: ");
+
+let primeNumber = true;
+for (let i = 2; i < number; i++) {
+  if (number % i == 0) {
+    primeNumber = false;
+    break;
+  }
+  primeNumber ? "Prime number" : " Not prime number";
+}
