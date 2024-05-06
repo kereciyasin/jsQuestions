4bb181276e6d18eb003a9b56441411c230766acf
Yasin Kereci <kereciyasin52@gmail.com>
2024-05-06 19:16:19 +0200

    end

--- for&while_questions/for&while.js
+++ for&while_questions/for&while.js
@@ -48,11 +48,11 @@
 // console.log(stars);
 
 
-for (let i = 1; i < 8; i++) {
-  let row = "";
-  for (let j = 0; j < i; j++) {
-    row += "*";
-  }
-  console.log(row);
-}
+// for (let i = 1; i < 8; i++) {
+//   let row = "";
+//   for (let j = 0; j < i; j++) {
+//     row += "*";
+//   }
+//   console.log(row);
+// }
 
