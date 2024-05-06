e138eb0556d01cd8b861d9da29c96ba62e25011f
Yasin Kereci <kereciyasin52@gmail.com>
2024-05-06 18:34:00 +0200

    chance

---
+++ for&while_questions/for&while.js
@@ -0,0 +1,14 @@
+//* Print out the numbers between 1000 and 2000 that leave a remainder of 3 when divided by 13.
+
+// for (let i = 1000; i < 2000; i++) {
+//   if (i % 13 == 3) {
+//     console.log(i);
+//   }
+// }
+
+//*  Print the average of 5 numbers obtained from the user to the console. 
+
+let counter = 0 
+while(counter<5){
+  console.log();
+}
