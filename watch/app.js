b3fd4fd27f2fd0cc68196d116064bf9667891df7
Yasin Kereci <kereciyasin52@gmail.com>
2024-05-14 12:39:58 +0200

    display

--- watch/app.js
+++ watch/app.js
@@ -1,1 +1,10 @@
-const display = document.querySelector(".clockDisplay")
+const display = document.querySelector(".clockDisplay");
+
+let date = new Date();
+console.log(date);
+let hour = date.getHours();
+let minute = date.getHours();
+let second = date.getHours();
+let session = "AM";
+
+display.innerHTML = hour + ":" + minute + ":" + second + ":" + session;
